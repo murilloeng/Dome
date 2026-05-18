9266e914da5e11b0910747ba2d5f05d19b7a1c69
Murillo Santana <mvbentosantana@gmail.com>
2026-05-18 08:10:46 +0200

    same as before

--- inc/Node.hpp
+++ inc/Node.hpp
@@ -12,9 +12,26 @@ public:
 	//destructor
 	~Node(void);
 
+	//data
+	uint32_t dof(uint32_t) const;
+
+	double state(uint32_t) const;
+	double state(uint32_t, double);
+
+	double position(uint32_t) const;
+	double position(uint32_t, double);
+
+	double velocity(uint32_t) const;
+	double velocity(uint32_t, double);
+
+	double acceleration(uint32_t) const;
+	double acceleration(uint32_t, double);
+
 private:
 	//data
-	double m_dof[3];
+	uint32_t m_dof[3];
+	double m_state[3];
 	double m_position[3];
-	uint32_t m_dof_indexes[3];
+	double m_velocity[3];
+	double m_acceleration[3];
 };
--- src/Node.cpp
+++ src/Node.cpp
@@ -2,7 +2,7 @@
 #include "Domes/inc/Node.hpp"
 
 //constructor
-Node::Node(void) : m_dof{0, 0, 0}, m_position{0, 0, 0}, m_dof_indexes{0, 0, 0}
+Node::Node(void) : m_dof{0, 0, 0}, m_state{0, 0, 0}, m_position{0, 0, 0}, m_velocity{0, 0, 0}, m_acceleration{0, 0, 0}
 {
 	return;
 }
@@ -11,4 +11,46 @@ Node::Node(void) : m_dof{0, 0, 0}, m_position{0, 0, 0}, m_dof_indexes{0, 0, 0}
 Node::~Node(void)
 {
 	return;
+}
+
+//data
+uint32_t Node::dof(uint32_t index) const
+{
+	return m_dof[index];
+}
+
+double Node::state(uint32_t index) const
+{
+	return m_state[index];
+}
+double Node::state(uint32_t index, double state)
+{
+	return m_state[index] = state;
+}
+
+double Node::position(uint32_t index) const
+{
+	return m_position[index];
+}
+double Node::position(uint32_t index, double position)
+{
+	return m_position[index] = position;
+}
+
+double Node::velocity(uint32_t index) const
+{
+	return m_velocity[index];
+}
+double Node::velocity(uint32_t index, double velocity)
+{
+	return m_velocity[index] = velocity;
+}
+
+double Node::acceleration(uint32_t index) const
+{
+	return m_acceleration[index];
+}
+double Node::acceleration(uint32_t index, double acceleration)
+{
+	return m_acceleration[index] = acceleration;
 }
