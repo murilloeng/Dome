5f0a453b8eefd1661e57276761b1f177115e19a6
Murillo Santana <mvbentosantana@gmail.com>
2026-05-18 08:10:37 +0200

    added some access functions

--- inc/Element.hpp
+++ inc/Element.hpp
@@ -16,6 +16,11 @@ public:
 	uint32_t node(uint32_t) const;
 	uint32_t node(uint32_t, uint32_t);
 
+	//formulation
+	void inertia(double*) const;
+	void stiffness(double*) const;
+	void internal_force(double*) const;
+
 private:
 	//data
 	uint32_t m_nodes[2];
